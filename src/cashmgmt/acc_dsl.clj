32c9f9ad28cf950d2fc6a561cb32e7b60bb7e901
clifford goldberg <clifford.goldberg@gmail.com>
2013-03-18 08:52:32 +0200

    balance checking direct and via db

--- src/cashmgmt/acc_dsl.clj
+++ src/cashmgmt/acc_dsl.clj
@@ -44,28 +44,70 @@
               :db/valueType :db.type/bigdec,
               :db/cardinality :db.cardinality/one,
               :db.install/_attribute :db.part/db}
-             {:db/id #db/id [:db.part/db],
-              :db/ident :balance-setter
-              :db/fn #db/fn {:lang :clojure
-                             :params [db id amount]
-                             :code (let [e (d/entity db id)
-                                         min-bal (:account/min-balance e 0)
-                                         max-bal (:account/max-balance e 0)
-                                         forecast-bal (+ (:account/balance e 0) amount)]
-                                     (if (and (>= forecast-bal min-bal) (<= forecast-bal max-bal))
-                                       [[:db/add id :account/balance forecast-bal]]
-                                       (throw (Exception. (str "illegal balance: " forecast-bal)))))}}
+
              [:db/add #db/id [:db.part/user] :db/ident :account.tag/avail]
              [:db/add #db/id [:db.part/user] :db/ident :account.tag/reserved]
 
              [:db/add #db/id [:db.part/user] :db/ident :account.instrument/p1]
              ])
+
 @(d/transact conn schema)
+(if (>= forecast-bal min-bal) ;;(<= forecast-bal max-bal)
+                                       [[:db/add id :account/balance forecast-bal]]
+                                       (throw (Exception. (str "illegal balance: " forecast-bal))))
+(defn balances [acc-ref]
+  (d/q '[:find ?b ?mnb ?mxb :in $ ?r :where
+        [?e :account/reference ?r]
+        [?e :account/balance ?b]
+        [?e :account/min-balance ?mnb]
+        [?e :account/max-balance ?mxb]] (db conn) acc-ref))
+(balances "a123")
 
-(d/q '[:find ?b :in $ ?r :where [?e :account/reference ?r] [?e :account/balance ?b]] (db conn) "a123")
+(def a123 (first (first (q '[:find ?e :where [?e :account/reference "a123"]] (db conn)))))
 
-(let [txid (d/tempid :db.part/tx)]
-  (d/transact conn [[:balance-setter txid 11M]]))
+(def balance-checker 
+  #db/fn {:lang :clojure
+          :params [db id amount]
+          :code (let [e (d/entity db id)
+                      min-bal (:account/min-balance e 0)
+                      max-bal (:account/max-balance e 0)
+                      forecast-bal (+ (:account/balance e 0) amount)]
+                  (println "forecast-bal: " forecast-bal "min-bal: " min-bal)
+                  (cond
+                   (< forecast-bal min-bal)
+                   {:account-balance [(format "balance: %.2f must be greater than min-balance: %.2f" forecast-bal min-bal)]}
+                   (> forecast-bal max-bal)
+                   {:account-balance [(format "balance: %.2f must be less than max-balance: %.2f" forecast-bal max-bal)]})
+                  )}
+  )
+
+
+;; balance is within bounds, should return nil
+(balances "a123")
+(balance-checker (db conn) a123 5M)
+(balance-checker (db conn) a123 15M)
+(balance-checker (db conn) a123 -500M)
+(balance-checker (db conn) a123 -2M)
+
+;; add balance-checker to the db
+(d/transact conn [{:db/id (d/tempid :db.part/user)
+                   :db/ident :account/balance-checker
+                   :db/fn balance-checker}])
+
+(def adj-bal-by 
+  #db/fn {:lang :clojure
+                             :params [db id amount]
+                             :code (let [e (d/entity db id)
+                                         forecast-bal (+ (:account/balance e 0) amount)]
+                                     (if-let [errors (d/invoke db :account/balance-checker db id amount)]
+                                       (throw ex-info "validation failed with errors" errors)
+                                       [[:db/add id :account/balance forecast-bal]])
+                                     )})
+
+(-> (adj-bal-by (db conn) a123 21M) util/should-throw)
+(adj-bal-by (db conn) a123 3M)
+
+;; should now have a history of balances for a123
 
 (let [txid (d/tempid :db.part/tx)]
   (d/transact conn [
