74affa363cc290cef1adf9566bb1d4ba9edee2ec
clifford goldberg <clifford.goldberg@gmail.com>
2013-03-25 08:29:49 +0200

    introduce operational transactions

--- src/cashmgmt/.#accounts.clj
+++
@@ -1,1 +0,0 @@
-dev@Cliffords-MacBook-Air.local.28131
--- src/cashmgmt/acc_dsl.clj
+++ src/cashmgmt/acc_dsl.clj
@@ -1,5 +1,6 @@
 (ns cashmgmt.acc-dsl
-  (:use [datomic.api :only [q db] :as d])
+  (:use [datomic.api :only [q db] :as d]
+        [clojure.pprint])
   (:require [datomic.samples.repl :as util])
   (:import [java.lang Exception]))
 
@@ -43,6 +44,47 @@
               :db/ident :account/max-balance,
               :db/valueType :db.type/bigdec,
               :db/cardinality :db.cardinality/one,
+              :db.install/_attribute :db.part/db}
+             {:db/id #db/id [:db.part/db],
+              :db/ident :account/transactions,
+              :db/valueType :db.type/ref,
+              :db/cardinality :db.cardinality/many,
+              :db.install/_attribute :db.part/db}
+
+             ;; operational transactions
+             { :db/id #db/id[:db.part/db]
+              :db/ident :ot/note
+              :db/valueType :db.type/string
+              :db/cardinality :db.cardinality/one
+              :db/doc "An note about what was transfered"
+              :db.install/_attribute :db.part/db}
+
+             { :db/id #db/id[:db.part/db]
+              :db/ident :ot/amount
+              :db/valueType :db.type/bigdec
+              :db/cardinality :db.cardinality/one
+              :db/doc "Amount transacted"
+              :db.install/_attribute :db.part/db}
+
+             { :db/id #db/id[:db.part/db]
+              :db/ident :ot/txtype
+              :db/valueType :db.type/ref
+              :db/cardinality :db.cardinality/one
+              :db/doc "classification of transaction"
+              :db.install/_attribute :db.part/db}
+
+             { :db/id #db/id[:db.part/db]
+              :db/ident :ot/dr
+              :db/valueType :db.type/ref
+              :db/cardinality :db.cardinality/one
+              :db/doc "Transferee"
+              :db.install/_attribute :db.part/db}
+
+             { :db/id #db/id[:db.part/db]
+              :db/ident :ot/cr
+              :db/valueType :db.type/ref
+              :db/cardinality :db.cardinality/one
+              :db/doc "Recipient"
               :db.install/_attribute :db.part/db}
 
              [:db/add #db/id [:db.part/user] :db/ident :account.tag/avail]
@@ -104,10 +146,63 @@
                                        [[:db/add id :account/balance forecast-bal]])
                                      )})
 
+(def tx-instants (reverse (sort (d/q '[:find ?when :where [_ :db/txInstant ?when]]
+                                     (d/db conn)))))
+tx-instants
+(def pre-adj-date (ffirst tx-instants))
+pre-adj-date
+
 (-> (adj-bal-by (db conn) a123 21M) util/should-throw)
 (adj-bal-by (db conn) a123 3M)
 
-;; should now have a history of balances for a123
+;; db val before adjusting balance
+(def dbval (db conn))
+
+;; install the adj-bal-by fn into the db
+(d/transact conn [{:db/id (d/tempid :db.part/user)
+                   :db/ident :account/adj-bal-by
+                   :db/fn adj-bal-by}])
+
+(d/transact conn [[:account/adj-bal-by a123 -1M]])
+(balances "a123")
+
+
+(pprint (sort #(compare (last %1) (last %2))
+         (seq (q '[:find ?e ?b ?t ?tx
+                    :in $ 
+                    :where
+                    [?tx :db/txInstant ?t]
+                    [?e :account/balance ?b ?tx]]
+                  (-> (db conn)
+                      ;;                    (d/since #inst "2013-03-16T11:34:15.925-00:00")
+                      (d/since #inst "2013-03-16")
+                      )))))
+
+(defn balance-deltas-since [t]
+  (let [db (-> (db conn) (d/since t))]
+    (seq (q '[:find ?e ?b ?t
+              :where
+             [?tx :db/txInstant ?t]
+             [?e :account/balance ?b ?tx]]
+           db))))
+
+(pprint (balance-deltas-since 13194139534320))
+
+(distinct (map :e (d/datoms (d/since (db conn) #inst "2013-03-16T11:34:15.925-00:00") :eavt)))
+
+;; now use clojure threading macros
+(def changed (->> (-> (db conn)
+          (d/since #inst "2013-03-16T11:34:15.925-00:00")
+          (d/datoms :eavt))
+      (map :e)
+      distinct))
+
+(pprint (map #(seq (d/entity (db conn) %)) changed))
+
+
+
+
+
 
 (let [txid (d/tempid :db.part/tx)]
   (d/transact conn [
@@ -121,5 +216,5 @@
                     ]))
 
 
-(def acc-a {:primary "xyz corp"
-            })
+
+;; emacs search for currently selected word - C-s C-w C-s
