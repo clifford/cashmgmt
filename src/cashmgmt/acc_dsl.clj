4b162b0fc4a2748f86c5bd24599308ee8dff1083
clifford goldberg <clifford.goldberg@gmail.com>
2013-06-05 08:55:13 +0200

    only the function definitions remain

--- src/cashmgmt/acc_dsl.clj
+++ src/cashmgmt/acc_dsl.clj
@@ -8,18 +8,9 @@
             [datomic.samples.query :as qry])
   (:import [java.lang Exception]))
 
-;; xyz -- operational -- service accs (fees, commission, ...)
-;;                    -- trade accs
-;;                    -- control accs
-;;     -- client/liability accs
-
-
-(def conn (util/scratch-conn))
-(def schema (read-string (slurp (io/resource "cashmgmt/cashmgmt-schema.edn"))))
-;; (def schema (read-string (slurp  "resources/cashmgmt/cashmgmt-schema.edn")))
-schema
-(println "********************" (dio/read-all schema))
-(d/transact conn schema)
+;; (def conn (util/scratch-conn))
+;; (def schema (read-string (slurp (io/resource "cashmgmt/cashmgmt-schema.edn"))))
+;; (d/transact conn schema)
 
 ;; instrument
 (defn create-instrument [conn instr types]
@@ -33,8 +24,8 @@ schema
 
 (defn get-instrument [dbval ref]
   (qry/find-by dbval :instrument/reference ref))
-(def b133 (create-instrument conn "b133" [:bond :fixed-income]))
-(def b133 (get-instrument (d/db conn) "b133"))
+;; (def b133 (create-instrument conn "b133" [:bond :fixed-income]))
+;; (def b133 (get-instrument (d/db conn) "b133"))
 
 ;; create account
 (defn create-acc [conn ref bal instr]
@@ -50,17 +41,17 @@ schema
                            ])
         (tx-ent txid))))
 
-(def a123 (create-acc conn "a123" 3M (qry/e b133)))
-(def a456 (create-acc conn "a456" 7M (qry/e b133)))
+;; (def a123 (create-acc conn "a123" 3M (qry/e b133)))
+;; (def a456 (create-acc conn "a456" 7M (qry/e b133)))
 
 (defn get-acc [conn ref]
   (->> (q '[:find ?e :where [?e :account/reference "a123"]] (db conn))
        ffirst
        (d/entity (d/db conn))))
 
-(:account/tag (get-acc conn "a123"))
-(def a123 (get-acc conn "a123"))
-(:db/id (d/db conn) a456) ;; same result from :db/id and d/entid
+;; (:account/tag (get-acc conn "a123"))
+;; (def a123 (get-acc conn "a123"))
+;; (:db/id (d/db conn) a456) ;; same result from :db/id and d/entid
 
 (defn balances [acc-ref]
   (d/q '[:find ?b ?mnb ?mxb :in $ ?r :where
@@ -68,7 +59,7 @@ schema
         [?e :account/balance ?b]
         [?e :account/min-balance ?mnb]
         [?e :account/max-balance ?mxb]] (db conn) acc-ref))
-(balances "a123")
+;; (balances "a123")
 
 (defn balance
   [db acc-id initial-balance]
@@ -87,7 +78,7 @@ schema
     (+ initial-balance
        (apply + (map first adds))
        (- (apply + (map first subtracts))))))
-(balance (d/db conn) (:db/id a123) 0M)
+;; (balance (d/db conn) (:db/id a123) 0M)
 
 (def balance-checker
   #db/fn {:lang :clojure
@@ -108,17 +99,17 @@ schema
 
 
 ;; balance is within bounds, should return nil
-(balances "a123")
-(d/entity (db conn) (:db/id a123))
-(balance-checker (db conn) a123 5M)
-(balance-checker (db conn) a123 15M)
-(balance-checker (db conn) a123 -500M)
-(balance-checker (db conn) a123 -2M)
+;; (balances "a123")
+;; (d/entity (db conn) (:db/id a123))
+;; (balance-checker (db conn) a123 5M)
+;; (balance-checker (db conn) a123 15M)
+;; (balance-checker (db conn) a123 -500M)
+;; (balance-checker (db conn) a123 -2M)
 
 ;; add balance-checker to the db
-(d/transact conn [{:db/id (d/tempid :db.part/user)
-                   :db/ident :account/balance-checker
-                   :db/fn balance-checker}])
+;; (d/transact conn [{:db/id (d/tempid :db.part/user)
+;;                    :db/ident :account/balance-checker
+;;                    :db/fn balance-checker}])
 
 ;; validate that the amt will keep balance within bounds. adjust the
 ;; balance for the account
@@ -133,22 +124,15 @@ schema
                     [[:db/add eid :account/balance forecast-bal]])
                   )})
 
-(-> (credit (db conn) a123 21M) util/should-throw)
-(credit (db conn) a123 3M)
-(credit (db conn) (:db/id a123) 3M)
-
-;; db val before adjusting balance
-(def dbval (db conn))
+;; (-> (credit (db conn) a123 21M) util/should-throw)
+;; (credit (db conn) a123 3M)
+;; (credit (db conn) (:db/id a123) 3M)
 
 ;; install the credit fn into the db
-(d/transact conn [{:db/id (d/tempid :db.part/user)
-                   :db/ident :account/credit
-                   :db/fn credit}])
+;; (d/transact conn [{:db/id (d/tempid :db.part/user)
+;;                    :db/ident :account/credit
+;;                    :db/fn credit}])
 
-(def ea123 (d/entity dbval (datomic.samples.query/e a123)))
-(:account/balance ea123)
-(d/transact conn [[:account/credit a123  -1M]])
-(balances "a123")
 
 ;; function to install all db functions into db
 (defn install-db-funcs [db]
@@ -172,10 +156,10 @@ schema
                       {:db/id txid :ot/note note :ot/dr fromid :ot/cr toid
                        :ot/txtype :ot.txtype/capital :ot/amount amount}])))
 
-(transfer conn a456 a123 1M "pay debt")
-(transfer conn (:db/id a456) (:db/id a123) 1M "pay debt")
-(balances "a123")
-(balances "a456")
+;; (transfer conn a456 a123 1M "pay debt")
+;; (transfer conn (:db/id a456) (:db/id a123) 1M "pay debt")
+;; (balances "a123")
+;; (balances "a456")
 
 ;; find all ot's for an account
 (defn txns [acc-ref]
@@ -193,35 +177,21 @@ schema
       (db conn)
       acc-ref)))
 
-(txns "a123")
-
-(def v (q '[:find ?txns ?when
-      :in $ ?ar
-      :where
-      [?a :account/reference ?ar]
-      [?a :account/transactions ?txns ?tx]
-      [?tx :db/txInstant ?when]]
-
-    (db conn)
-    "a123"))
-v
 
 (def tx-instants (reverse (sort (d/q '[:find ?when :where [_ :db/txInstant ?when]]
                                      (d/db conn)))))
-tx-instants
 
 
-
-(pprint (sort #(compare (last %1) (last %2))
-         (seq (q '[:find ?e ?b ?t ?tx
-                    :in $
-                    :where
-                    [?tx :db/txInstant ?t]
-                    [?e :account/balance ?b ?tx]]
-                  (-> (db conn)
-                      ;;                    (d/since #inst "2013-03-16T11:34:15.925-00:00")
-                      (d/since #inst "2013-03-16")
-                      )))))
+;; (pprint (sort #(compare (last %1) (last %2))
+;;          (seq (q '[:find ?e ?b ?t ?tx
+;;                     :in $
+;;                     :where
+;;                     [?tx :db/txInstant ?t]
+;;                     [?e :account/balance ?b ?tx]]
+;;                   (-> (db conn)
+;;                       ;;                    (d/since #inst "2013-03-16T11:34:15.925-00:00")
+;;                       (d/since #inst "2013-03-16")
+;;                       )))))
 
 (defn balance-deltas-since [t]
   (let [db (-> (db conn) (d/since t))]
@@ -231,9 +201,10 @@ tx-instants
              [?e :account/balance ?b ?tx]]
            db))))
 
-(pprint (balance-deltas-since 13194139534320))
+;; (pprint (balance-deltas-since 13194139534320))
 
-(distinct (map :e (d/datoms (d/since (db conn) #inst "2013-03-16T11:34:15.925-00:00") :eavt)))
+;; using the datomic indexes directly
+;; (distinct (map :e (d/datoms (d/since (db conn) #inst "2013-03-16T11:34:15.925-00:00") :eavt)))
 
 ;; now use clojure threading macros
 (def changed (->> (-> (db conn)
@@ -242,4 +213,4 @@ tx-instants
       (map :e)
       distinct))
 
-(pprint (map #(seq (d/entity (db conn) %)) changed))
+;; (pprint (map #(seq (d/entity (db conn) %)) changed))
